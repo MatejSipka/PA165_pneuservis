2509be6a94baa572bf254fa26b17f3562dc699ed
kyberpunk <xvh@seznam.cz>
2016-12-16 13:15:31 +0100

    Order view action + view

--- SpringMVC/src/main/java/cz/fi/muni/pa165/pneuservis/controllers/OrderController.java
+++ SpringMVC/src/main/java/cz/fi/muni/pa165/pneuservis/controllers/OrderController.java
@@ -4,11 +4,13 @@ import cz.fi.muni.pa165.pneuservis.dto.OrderDTO;
 import cz.fi.muni.pa165.pneuservis.dto.PersonDTO;
 import cz.fi.muni.pa165.pneuservis.facade.OrderFacade;
 import cz.fi.muni.pa165.pneuservis.facade.PersonFacade;
+import cz.fi.muni.pa165.pneuservis.rest.exceptions.ForbiddenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -51,5 +53,22 @@ public class OrderController {
         }
         model.addAttribute("orders", orders);
         return "order/list";
+    }
+
+    @RequestMapping(value = "/{id}/view", method = RequestMethod.GET)
+    public String view(@PathVariable long id, Model model) {
+        OrderDTO order = orderFacade.findOrderById(id);
+        PersonDTO person = PersonDTO.class.cast(session.getAttribute("authenticated"));
+        if (person != null) {
+            if (personFacade.findById(person.getId()).getPersonType() == EMPLOYEE) {
+                model.addAttribute("Admin", person.getLogin());
+            } else {
+                if (order.getClientId() != person.getId()) throw new ForbiddenException();
+                model.addAttribute("User", person.getLogin());
+            }
+        }
+        model.addAttribute("person", person);
+        model.addAttribute("order", order);
+        return "order/view";
     }
 }
